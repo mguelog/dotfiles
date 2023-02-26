26a2febfdba8b4962e545785fda9ee30aa097faa
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-02-26 14:58:17 +0100

    feat(display manager): lightdm

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ sudo pacman -S xorg-server
 localectl set-x11-keymap es
 sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/
 sudo cp 50-mouse-acceleration.conf /etc/X11/xorg.conf.d/
+
+
+# Display manager
+sudo pacman -S lightdm lightdm-gtk-greeter
+systemctl enable lightdm
