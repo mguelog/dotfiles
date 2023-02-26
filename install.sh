a9e8587ef260cb4b9c7d63386063cbc1c4018ab5
Miguel Oña Guerrero <100541741+mguelog@users.noreply.github.com>
2023-02-26 14:34:07 +0100

    feat(display server): xorg

---
+++ install.sh
@@ -0,0 +1,8 @@
+
+# Display server
+sudo pacman -S xorg-server
+
+# Input devices configuration
+localectl set-x11-keymap es
+sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/
+sudo cp 50-mouse-acceleration.conf /etc/X11/xorg.conf.d/
